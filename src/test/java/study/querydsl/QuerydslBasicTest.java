7b36c70e5d260662bb04feeb9ee920912039b29b
seheonnn <ho78901@naver.com>
2024-06-21 21:48:45 +0900

    test: 조인과 on절

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -243,4 +243,93 @@ public class QuerydslBasicTest {
 		assertThat(teamB.get(team.name)).isEqualTo("teamB");
 		assertThat(teamB.get(member.age.avg())).isEqualTo(35); // (30 + 40) / 2
 	}
+
+	/**
+	 * 팀 A에 소속된 모든 회원
+	 */
+	@Test
+	public void join() {
+		List<Member> result = queryFactory
+			.selectFrom(member)
+			.join(member.team, team)
+			.where(team.name.eq("teamA"))
+			.fetch();
+
+		assertThat(result)
+			.extracting("username")
+			.containsExactly("member1", "member2");
+	}
+
+	/**
+	 * 세타 조인
+	 * 회원의 이름이 팀 이름과 같은 회원 조회 (전혀 상관없는 조인)
+	 */
+	@Test
+	public void theta_join() {
+		em.persist(new Member("teamA"));
+		em.persist(new Member("teamB"));
+		em.persist(new Member("teamC"));
+
+		List<Member> result = queryFactory
+			.select(member)
+			.from(member, team)
+			.where(member.username.eq(team.name))
+			.fetch();
+
+		assertThat(result)
+			.extracting("username")
+			.containsExactly("teamA", "teamB");
+	}
+
+	/**
+	 * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+	 * JPQL: select m, t from Member m left join m.team t on t.name = 'teamA'
+	 */
+	@Test
+	public void join_on_filtering() {
+		List<Tuple> result = queryFactory
+			.select(member, team)
+			.from(member)
+			.leftJoin(member.team, team)
+			.on(team.name.eq("teamA")) // leftJoin, rightJoin 의 경우 where 로는 불가. on 만 가능
+			.fetch();
+
+		// 그냥 join (innerJoin)인 경우 on 절은 where 절에서 필터링 하는 것과 같은 효과
+		// List<Tuple> result = queryFactory
+		// 	.select(member, team)
+		// 	.from(member)
+		// 	.join(member.team, team)
+		// 	// .on(team.name.eq("teamA"))
+		// 	.where(team.name.eq("teamA"))
+		// 	.fetch();
+
+		for (Tuple tuple : result) {
+			System.out.println("tuple=" + tuple);
+		}
+	}
+
+	/**
+	 * 연관관계 없는 엔티티 외부 조인
+	 * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+	 */
+	@Test
+	public void join_on_no_relation() {
+		em.persist(new Member("teamA"));
+		em.persist(new Member("teamB"));
+		em.persist(new Member("teamC"));
+
+		List<Tuple> result = queryFactory
+			.select(member, team)
+			.from(member)
+			.leftJoin(team).on(member.username.eq(team.name))
+			.where(member.username.eq(team.name))
+			.fetch();
+
+		for (Tuple tuple : result) {
+			System.out.println("tuple=" + tuple);
+		}
+		// 주의! 문법을 잘 봐야 한다. **leftJoin()** 부분에 일반 조인과 다르게 엔티티 하나만 들어간다.
+		// 일반적인 leftJoin: `leftJoin(member.team, team)`
+		// on 절을 이용한 외부조인: `from(member).leftJoin(team).on(xxx)`
+	}
 }
