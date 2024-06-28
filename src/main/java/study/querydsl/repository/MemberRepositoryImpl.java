1cd53b1049b54021c895d5a899e01c92b1b768b8
seheonnn <ho78901@naver.com>
2024-06-28 15:37:17 +0900

    refactor: countQuery 최적화

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -6,13 +6,21 @@ import static study.querydsl.entity.QTeam.*;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import jakarta.persistence.EntityManager;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
@@ -56,5 +64,95 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
 	private BooleanExpression ageLoe(Integer ageLoe) {
 		return ageLoe != null ? member.age.loe(ageLoe) : null;
+	}
+
+	@Override
+	public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+		QueryResults<MemberTeamDto> results = queryFactory
+			.select(new QMemberTeamDto(
+				member.id.as("memberId"),
+				member.username,
+				member.age,
+				team.id.as("teamId"),
+				team.name.as("teamName")))
+			.from(member)
+			.leftJoin(member.team, team)
+			.where(
+				usernameEq(condition.getUsername()),
+				teamNameEq(condition.getTeamName()),
+				ageGoe(condition.getAgeGoe()),
+				ageLoe(condition.getAgeLoe())
+			)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetchResults();
+
+		// fetchResults -> contents 를 위한 쿼리 + Count 를 위한 쿼리 = 쿼리 두 번
+		// fetchResults 에서 orderBy 는 count 쿼리에선 지워짐
+
+		List<MemberTeamDto> content = results.getResults();
+		long total = results.getTotal();
+
+		return new PageImpl<>(content, pageable, total);
+
+	}
+
+	// content 와 count 별도로 조회
+	// 상황에 따라 content 에 비하여 count 쿼리가 단순할 수 있음. 최적화 가능 -> 성능 개선
+	// fetchResult 는 content 쿼리의 조건들이 대부분 count 쿼리에도 붙기 때문에 성능상 불리
+
+	// 아니면 count 쿼리를 먼저 호출하고 만약 데이터가 없다면 content 쿼리는 호출하지 않는 등... 의 성능 개선 가능
+	@Override
+	public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+		List<MemberTeamDto> content = queryFactory
+			.select(new QMemberTeamDto(
+				member.id.as("memberId"),
+				member.username,
+				member.age,
+				team.id.as("teamId"),
+				team.name.as("teamName")))
+			.from(member)
+			.leftJoin(member.team, team)
+			.where(
+				usernameEq(condition.getUsername()),
+				teamNameEq(condition.getTeamName()),
+				ageGoe(condition.getAgeGoe()),
+				ageLoe(condition.getAgeLoe())
+			)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		// fetch -> content 조회 쿼리 하나만
+
+		// count 쿼리 따로 구현
+		// long total = queryFactory
+		// 	.select(member)
+		// 	.from(member)
+		// 	.leftJoin(member.team, team)
+		// 	.where(
+		// 		usernameEq(condition.getUsername()),
+		// 		teamNameEq(condition.getTeamName()),
+		// 		ageGoe(condition.getAgeGoe()),
+		// 		ageLoe(condition.getAgeLoe())
+		// 	)
+		// 	.fetchCount();
+
+		// return new PageImpl<>(content, pageable, total);
+
+		// count 쿼리 최적화
+		JPAQuery<Member> countQuery = queryFactory
+			.select(member)
+			.from(member)
+			.leftJoin(member.team, team)
+			.where(
+				usernameEq(condition.getUsername()),
+				teamNameEq(condition.getTeamName()),
+				ageGoe(condition.getAgeGoe()),
+				ageLoe(condition.getAgeLoe())
+			);
+
+		// content 와 pageable 을 보고 content 사이즈가 (page 시작임에도) pageSize 보다 작거나 마지막 페이지라면 countQuery 안 날림
+		return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
 	}
 }
