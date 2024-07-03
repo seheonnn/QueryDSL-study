8b52c912e138c64a5bc1c75913168967442bd81d
seheonnn <ho78901@naver.com>
2024-07-03 21:27:41 +0900

    feat: QuerydslPredicateExecutor 적용

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -3,11 +3,16 @@ package study.querydsl.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import study.querydsl.entity.Member;
 
 // 인터페이스 상속 여러개 가능
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+// QuerydslPredicateExecutor<Member> 는 실무에서 거의 사용 불가
+// 한계) 묵시적 조인은 가능하지만 left join 은 불가
+// 클라이언트 코드가 QueryDsl 에 의존. 사용시 QMember 필요
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom,
+	QuerydslPredicateExecutor<Member> {
 
 	// JpaRepository 가 메서드 명으로 쿼리 만듦 -> select m from Member m where m.username = ?
 	List<Member> findByUsername(String username);
